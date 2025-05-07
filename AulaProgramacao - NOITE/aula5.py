20c4d0b4706f41359bcea47ef561c18b33f5ad47
MamedeXd <grmamede@gmail.com>
2025-05-06 21:51:16 -0300

    Add files via upload

---
+++ AulaProgramacao - NOITE/aula5.py
@@ -0,0 +1,16 @@
+# (>) maior  (<) menor  (>=) maior ou igual  (<=) menor ou igual  [(!=) diferente  (==) igualdade {texto}]
+
+'''
+num1: int = 7
+num2: int = 77
+resultado: bool = False
+resultado = num1 > num2
+print(resultado)
+'''
+
+idade: int = int(input("Digite sua idade: "))
+
+if idade >= 18:
+    print("Bem-Vindo à Festa!!");
+else:
+    print("Sentou na graxa kkakakaka")
