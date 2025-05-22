b8fb728afbe3776cf862e266085707861066ab66
MamedeXd <grmamede@gmail.com>
2025-05-22 20:33:36 -0300

    Add files via upload

---
+++ AulaProgramacao - Python/Aulas e Desafios/desafio_aula11.py
@@ -0,0 +1,22 @@
+'''
+Crie um programa que receba uma palavra e uma letra.
+Conte quantas vezes a letra digitada aparece na palavra.
+'''
+
+palavra: str = input("informe a palavra: ")
+letra_buscada: str = input("Informe a letra a ser buscada: ")
+total: int = 0
+for letra in palavra:
+    print("A letra atual: ", letra)
+    if letra == letra_buscada:
+        total = total + 1
+print(f"O total de {letra_buscada} na palavra {palavra} é {total}.")
+
+
+'''
+palavra: str = input("Digite uma palavra: ")
+letra: str = input("Digite a letra a ser contada: ")
+quantidade = palavra.count(letra)
+
+print(f"A letra {letra} aparece {quantidade} vezes.")
+'''
